ceb07789b98d68a24d65e3ce11e08fb61156e5bc
Arpitshivam Pandey <arpitshivampandey@gmail.com>
2024-07-16 12:00:04 +0530

    Option and exception handeling

--- scala/Assignment/Ownlist.scala
+++ scala/Assignment/Ownlist.scala
@@ -1,44 +1,44 @@
 package Assignment
 
-object Ownlist extends App{
-  val empty = new Empty
 
-  val list = empty.add(1).add(2).add(3).add(4).add(5)
+//  val empty = new Cons(1,)
+//
+//  val list = empty.add(1)
+//
+////  println(list)
+//
+//  println(list.head)
+//  println(list.tail)
+////  println(list.tail.head)
+////  println(list.tail.tail)
+////
+////  println(list.tail.tail.head)
+//}
+//abstract class Mylist{
+//  def head: Int
+//  def tail: Mylist
+//  def isEmpty: Boolean
+//  def add(x: Int): Mylist
+////  def remove(x: Int): Mylist
+//  def printElements: String
 
-  println(list)
-
-  println(list.head)
-  println(list.tail)
-  println(list.tail.head)
-  println(list.tail.tail)
-
-  println(list.tail.tail.head)
-}
-abstract class Mylist{
-  def head: Int
-  def tail: Mylist
-  def isEmpty: Boolean
-  def add(x: Int): Mylist
-//  def remove(x: Int): Mylist
-  def printElements: String
-
-  override def toString: String = "[" + printElements + "]"
-}
-
-class Empty extends Mylist{
-  def head:Int= throw new NoSuchElementException()
-  def tail:Mylist= throw new NoSuchElementException
-  def isEmpty: Boolean = true
-  def add(x: Int): Mylist = new Cons(x,this)
-//  def remove(x: Int): Mylist=MyList[Empty]
-  def printElements: String = " "
-}
-
-class Cons( h: Int, t: Mylist) extends Mylist{
-  def head: Int = h
-  def tail: Mylist = t
-  def isEmpty: Boolean = false
-  def add(x: Int): Mylist = new Cons(x,this)
+//  override def toString: String = "[" + printElements + "]"
+//}
+//
+//class Empty extends Mylist{
+//  def head:Int= throw new NoSuchElementException()
+//  def tail:Mylist= throw new NoSuchElementException
+//  def isEmpty: Boolean = true
+//  def add(x: Int): Mylist = new Cons(x,this)
+////  def remove(x: Int): Mylist=MyList[Empty]
+//  def printElements: String = " "
+//}
+//
+//class Cons( h: Int, t: Mylist) extends Mylist{
+//  def head: Int = h
+//  def tail: Mylist = t
+//  def isEmpty: Boolean = false
+//  def add(x: Int): Mylist = new Cons(x,this)
 //  def remove(x: Int): Mylist ={
 
 //    def helper(h:Int,t:MyList,ind:Int):Int={
@@ -47,9 +47,10 @@ class Cons( h: Int, t: Mylist) extends Mylist{
 //    }
 
 
+//
+//  def printElements: String = {
+//    if (tail.isEmpty) s"$head"
+//    else s"$head, ${tail.printElements}"
+//  }
 
-  def printElements: String = {
-    if (tail.isEmpty) s"$head"
-    else s"$head, ${tail.printElements}"
-  }
-}
+  
